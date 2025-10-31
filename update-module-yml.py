a18da04a70973424e3f48f85fa31359f145b5398
h4ck0lympus <vishaljuneja707@gmail.com>
2025-10-30 20:59:12 -0700

    fix

--- update-module-yml.py
+++ update-module-yml.py
@@ -1,20 +1,30 @@
 #!/usr/bin/env python3
 import yaml
 import os
+import re
 
 challenges = os.listdir("./basket")
 
+MAX_ID_LEN = 32
 
-with open("./basket/module.yml") as f:
-    data = yaml.safe_load(f)
+def sanitize_id(chall_id):
+    chall_id = chall_id.lower()
+    chall_id = re.sub(r'[^a-z0-9-]', '-', chall_id) # lowercase only and valid chars
+    return chall_id[:MAX_ID_LEN] # keep it under id limit
 
 def get_name(chall_id):
     parts = chall_id.split('-')
     parts = parts[1:]
     return ' '.join(word.capitalize() for word in parts)
 
+data = {
+    "id": "basket",
+    "name": "basket",
+}
+
 data["challenges"] = []
 for chall in sorted(challenges):
+    chall_id = sanitize_id(chall)
     name = get_name(chall)
     data["challenges"].append({
         "id": chall,
