aa23b085aaaf6ff6648f8e6e5a75179f1dd7cfbc
screwj00 <rcthor@asu.edu>
2025-11-03 01:59:46 -0700

    bye bye source code

--- basket/livectf2023-house-of-barbie/alloc.h
+++
@@ -1,7 +0,0 @@
-#include <stdint.h>
-#include <stddef.h>
-
-void* malloc(size_t size);
-void free(void* ptr);
-void* realloc(void* ptr, size_t size);
-void print_state();
--- basket/livectf2023-jammer/lib.h
+++
@@ -1,5 +0,0 @@
-
-char *generateRandomString(int length);
-unsigned int generateRandomNumber(int min, int max);
-int receiveUntil(char *buf, int length, char delim);
-int sendBytes(char *buffer, int length);
