e5ad80c10cdadd9a5192ee687c82122da75d14c6
h4ck0lympus <vishaljuneja707@gmail.com>
2026-02-17 16:26:57 -0700

    purge old module folders

--- hi/hi-8ab3f294/runme.c
+++
@@ -1,9 +0,0 @@
-#include <unistd.h>
-#include <stdio.h>
-
-int main() {
-	setuid(0);
-	setgid(0);
-	execl("/challenge/challenge", "/challenge/challenge", NULL);
-	return 0;
-}
--- hi/hi-8ecb23d1/runme.c
+++
@@ -1,9 +0,0 @@
-#include <unistd.h>
-#include <stdio.h>
-
-int main() {
-	setuid(0);
-	setgid(0);
-	execl("/challenge/run.sh", "/challenge/run.sh", NULL);
-	return 0;
-}
